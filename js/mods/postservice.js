7d42634055fb49681733a244417f2071f267c765
busticated <itsbusticated@gmail.com>
2012-07-17 16:17:55 -0700

    use real cheezburger post assets

--- js/mods/postservice.js
+++ js/mods/postservice.js
@@ -1,31 +1,21 @@
 /* global define: false, require: false */
 
-define( [ 'mods/fakeajaxservice' ], function( Fakejax ){
+define( [ 'mods/fakeajaxservice', 'mods/fakePostData' ], function( Fakejax, fakePostData ){
     'use strict';
 
-    var makePost = function( id ){
-        var title = 'I am post number ' + id,
-            body = (function(){
-                var lorem = [
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-                    'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-                    'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-                    'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-                    'Contrary to popular belief, Lorem Ipsum is not simply random text.',
-                    'It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
-                    'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
-                    'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC.',
-                    'This book is a treatise on the theory of ethics, very popular during the Renaissance.',
-                    'The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
-                ];
+    var makePost = function(){
+        var idx = random( 0, fakePostData.length - 1 ),
+            rawPost = fakePostData[ idx ],
+            id = rawPost.AssetId,
+            title = rawPost.Title,
+            body = rawPost.Description,
+            imageSrc = rawPost.Url,
+            imageHeight = rawPost.Height,
+            imageWidth = rawPost.Width;
 
-                var lines = lorem.slice( 0, random( 0, lorem.length - 1 ) );
-
-                return lines.join( ' ' );
-            })(),
-            imageWidth = 580,
-            imageHeight = ( random( 2, 12 ) * 100 ),
-            imageSrc = 'http://placekitten.com/' + imageWidth + '/' + imageHeight;
+        if ( rawPost.AssetType !== 0 || rawPost.Height === 0 ){
+            return makePost();
+        }
 
         return {
             id: id,
