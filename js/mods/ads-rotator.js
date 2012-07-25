bb5f4cc6b994176c2da0bdf4cde2ce02d11b3b9e
busticated <itsbusticated@gmail.com>
2012-07-24 18:28:10 -0700

    rollback ad-rotator slot name change

--- js/mods/ads-rotator.js
+++ js/mods/ads-rotator.js
@@ -10,12 +10,12 @@ define( [ 'jquery', 'mods/mastercontrol', 'mods/ads', 'mods/utils' ], function( 
         tmpl: [
             '<div class="ad-container ad-rotator">',
                 '<div id="js-adgroup-01" class="ad-group ad-group-a is-visible">',
-                    '<div id="ad-skyscraper-01" class="js-ad ad ad-left ad-skyscraper" data-adtype="ICHCTEST_160_LEFT_BTFISCROLL1_ROS" data-adsize="[[160, 600],[300,600]]"></div>',
-                    '<div id="ad-skyscraper-02" class="js-ad ad ad-right ad-skyscraper" data-adtype="ICHCTEST_160_RIGHT_BTFISCROLL1_ROS" data-adsize="[[160, 600],[300,600]]"></div>',
+                    '<div id="ad-skyscraper-01" class="js-ad ad ad-left ad-skyscraper" data-adtype="ICHCTEST_160_LEFT_BTFISCROLL_ROS_0" data-adsize="[[160, 600],[300,600]]"></div>',
+                    '<div id="ad-skyscraper-02" class="js-ad ad ad-right ad-skyscraper" data-adtype="ICHCTEST_160_RIGHT_BTFISCROLL_ROS_0" data-adsize="[[160, 600],[300,600]]"></div>',
                 '</div>',
                 '<div id="js-adgroup-02" class="ad-group ad-group-b">',
-                    '<div id="ad-skyscraper-03" class="js-ad ad ad-left ad-skyscraper" data-adtype="ICHCTEST_160_LEFT_BTFISCROLL2_ROS" data-adsize="[[160, 600],[300,600]]"></div>',
-                    '<div id="ad-skyscraper-04" class="js-ad ad ad-right ad-skyscraper" data-adtype="ICHCTEST_160_RIGHT_BTFISCROLL2_ROS" data-adsize="[[160, 600],[300,600]]"></div>',
+                    '<div id="ad-skyscraper-03" class="js-ad ad ad-left ad-skyscraper" data-adtype="ICHCTEST_160_LEFT_BTFISCROLL_ROS_1" data-adsize="[[160, 600],[300,600]]"></div>',
+                    '<div id="ad-skyscraper-04" class="js-ad ad ad-right ad-skyscraper" data-adtype="ICHCTEST_160_RIGHT_BTFISCROLL_ROS_1" data-adsize="[[160, 600],[300,600]]"></div>',
                 '</div>',
             '</div>'
         ].join( '' ),
