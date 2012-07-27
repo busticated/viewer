084e602f44c02590ad29a3ae6374a60341389a2a
busticated <itsbusticated@gmail.com>
2012-07-27 14:54:41 -0700

    enable sticky rotator container

--- js/mods/ads-rotator.js
+++ js/mods/ads-rotator.js
@@ -8,14 +8,16 @@ define( [ 'jquery', 'mods/mastercontrol', 'mods/ads', 'mods/utils' ], function( 
     a.options = {
         container: '#js-poststream',
         tmpl: [
-            '<div class="ad-container ad-rotator">',
-                '<div id="js-adgroup-01" class="ad-group ad-group-a is-visible">',
-                    '<div id="ad-skyscraper-01" class="js-ad ad ad-left ad-skyscraper" data-adtype="ICHCTEST_160_LEFT_BTFISCROLL_ROS_0" data-adsize="[[160, 600],[300,600]]"></div>',
-                    '<div id="ad-skyscraper-02" class="js-ad ad ad-right ad-skyscraper" data-adtype="ICHCTEST_160_RIGHT_BTFISCROLL_ROS_0" data-adsize="[[160, 600],[300,600]]"></div>',
-                '</div>',
-                '<div id="js-adgroup-02" class="ad-group ad-group-b">',
-                    '<div id="ad-skyscraper-03" class="js-ad ad ad-left ad-skyscraper" data-adtype="ICHCTEST_160_LEFT_BTFISCROLL_ROS_1" data-adsize="[[160, 600],[300,600]]"></div>',
-                    '<div id="ad-skyscraper-04" class="js-ad ad ad-right ad-skyscraper" data-adtype="ICHCTEST_160_RIGHT_BTFISCROLL_ROS_1" data-adsize="[[160, 600],[300,600]]"></div>',
+            '<div id="js-adrotator-wrap">',
+                '<div id="js-adrotator" class="ad-container ad-rotator">',
+                    '<div id="js-adgroup-01" class="ad-group ad-group-a is-visible">',
+                        '<div id="ad-01" class="js-ad ad ad-left ad-skyscraper" data-adtype="ICHCTEST_160_LEFT_BTFISCROLL_ROS" data-adsize="[[160, 600],[300,600]]"></div>',
+                        '<div id="ad-02" class="js-ad ad ad-right ad-skyscraper" data-adtype="ICHCTEST_160_RIGHT_BTFISCROLL_ROS" data-adsize="[[160, 600],[300,600]]"></div>',
+                    '</div>',
+                    '<div id="js-adgroup-02" class="ad-group ad-group-b">',
+                        '<div id="ad-03" class="js-ad ad ad-left ad-skyscraper" data-adtype="ICHCTEST_160_LEFT_BTFISCROLL_ROS_0" data-adsize="[[160, 600],[300,600]]"></div>',
+                        '<div id="ad-04" class="js-ad ad ad-right ad-skyscraper" data-adtype="ICHCTEST_160_RIGHT_BTFISCROLL_ROS_0" data-adsize="[[160, 600],[300,600]]"></div>',
+                    '</div>',
                 '</div>',
             '</div>'
         ].join( '' ),
@@ -31,6 +33,11 @@ define( [ 'jquery', 'mods/mastercontrol', 'mods/ads', 'mods/utils' ], function( 
 
     a.listen = function(){
         mc.on( 'ads.rotate', a.rotateAds );
+
+        $( '#js-adrotator-wrap' ).waypoint(function( e, direction ) {
+            e.stopPropagation();
+            $( '#js-adrotator' ).toggleClass( 'is-fixed', direction === 'down' );
+        }, { offset: 50, onlyOnScroll: true });
 
         return this;
     };
