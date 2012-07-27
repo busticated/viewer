2ec1218d482be5bdd7669a480a10cf4da739447c
busticated <itsbusticated@gmail.com>
2012-07-27 14:58:36 -0700

    expose waypoint offset setting as property on postView

--- js/mods/postView.js
+++ js/mods/postView.js
@@ -15,6 +15,7 @@ define( [ 'jquery', 'libs/handlebars', 'mods/view', 'libs/polyfills' ], function
         this.$el = $( this.tmpl( this.model ) ).data( 'postIndex', idx );
         this.innerHtml = this.$el[ 0 ].innerHTML;
         this.isClearedClass = 'is-cleared';
+        this.waypointOffset = 0;
 
         this.model.on( 'update:isActive', function( isActive ){
             if( isActive ){
@@ -37,7 +38,7 @@ define( [ 'jquery', 'libs/handlebars', 'mods/view', 'libs/polyfills' ], function
     PostView.prototype.constructor = PostView;
 
     PostView.prototype.setup = function(){
-        this.$el.appendTo( '#js-poststream' ).waypoint();
+        this.$el.appendTo( '#js-poststream' ).waypoint({ offset: this.waypointOffset });
         this.$el.data( 'postModel', this.model );
         return this;
     };
@@ -50,10 +51,11 @@ define( [ 'jquery', 'libs/handlebars', 'mods/view', 'libs/polyfills' ], function
     };
 
     PostView.prototype.remove = function(){
-        this.$el.height( this.$el.outerHeight() );
-        this.$el.addClass( this.isClearedClass );
+        var height = this.$el.outerHeight();
+
         this.$el.empty();
-        return this;
+        this.$el.css( 'minHeight', height );
+        this.$el.addClass( this.isClearedClass );
     };
 
     PostView.prototype.select = function( nudge ){
